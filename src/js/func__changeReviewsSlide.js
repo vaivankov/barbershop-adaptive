2eaf03e5d2ddd695990998edce38896eb51b39b1
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-08-27 14:35:12 +0300

    Добавлены webp изображения в функции смены слайдов

--- src/js/func__changeReviewsSlide.js
+++ src/js/func__changeReviewsSlide.js
@@ -1,24 +1,42 @@
 "use strict";
 
 (function () {
-	window.changeReviewsSlide = function () {
-		let review = reviewHub[currentReview];
-		let sourceDesktop = document.querySelector(".reviews__source-desktop");
-		let sourceTablet = document.querySelector(".reviews__source-tablet");
-		let sourceMobile = document.querySelector(".reviews__author-image");
-		let targetTitle = document.querySelector(".reviews__author-name");
-		let targetDescription = document.querySelector(".reviews__text");
+  window.changeReviewsSlide = function () {
+    let review = reviewHub[currentReview];
+    let sourceDesktopWEBP = document.querySelector(
+      ".reviews__source-desktop-webp"
+    );
+    let sourceDesktopJPG = document.querySelector(
+      ".reviews__source-desktop-jpg"
+    );
+    let sourceTabletWEBP = document.querySelector(
+      ".reviews__source-tablet-webp"
+    );
+    let sourceTabletJPG = document.querySelector(".reviews__source-tablet-jpg");
+    let sourceMobileWEBP = document.querySelector(
+      ".reviews__source-mobile-webp"
+    );
+    let sourceMobile = document.querySelector(".reviews__author-image");
+    let targetTitle = document.querySelector(".reviews__author-name");
+    let targetDescription = document.querySelector(".reviews__text");
 
-		sourceDesktop.srcset = `img/raster/${review.image}-desktop.jpg 1x, img/raster/${review.image}-desktop@2x.jpg 2x`;
-		sourceTablet.srcset = `img/raster/${review.image}-tablet.jpg 1x, img/raster/${review.image}-tablet@2x.jpg 2x`;
-		sourceMobile.src = `img/raster/${review.image}-mobile.jpg`;
-		sourceMobile.alt = review.alt;
-		sourceMobile.srcset = `img/raster/${review.image}-mobile@2x.jpg 2x`;
-		targetTitle.innerHTML = review.name;
-		targetDescription.innerHTML = review.text;
-	};
+    sourceDesktopWEBP.srcset = `img/${review.image}-desktop.webp 1x, img/${review.image}-desktop@2x.webp 2x`;
+    sourceDesktopJPG.srcset = `img/${review.image}-desktop.jpg 1x, img/${review.image}-desktop@2x.jpg 2x`;
+
+    sourceTabletWEBP.srcset = `img/${review.image}-tablet.webp 1x, img/${review.image}-tablet@2x.webp 2x`;
+    sourceTabletJPG.srcset = `img/${review.image}-tablet.jpg 1x, img/${review.image}-tablet@2x.jpg 2x`;
+
+    sourceMobileWEBP.srcset = `img/${review.image}-mobile@2x.webp 2x`;
+    sourceMobile.src = `img/${review.image}-mobile.jpg`;
+
+    sourceMobile.alt = review.alt;
+    sourceMobile.srcset = `img/${review.image}-mobile@2x.jpg 2x`;
+
+    targetTitle.innerHTML = review.name;
+    targetDescription.innerHTML = review.text;
+  };
 })();
 
 for (let a of reviewsRadioAll) {
-	a.addEventListener("input", changeReviewsSlide);
+  a.addEventListener("input", changeReviewsSlide);
 }
